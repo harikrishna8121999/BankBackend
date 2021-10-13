1e95d3de453930c39d5e8069dc28f15fc7e8a189
Harikrishna V Shetty <harikrishna8121999@gmail.com>
2021-10-13 20:45:39 +0530

    Api

--- src/main/java/com/example/BankBackend/controller/AccountDetailsController.java
+++ src/main/java/com/example/BankBackend/controller/AccountDetailsController.java
@@ -3,13 +3,21 @@ package com.example.BankBackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.BankBackend.exception.ResourceNotFoundException;
 import com.example.BankBackend.model.AccountDetails;
 import com.example.BankBackend.repository.AccountRepository;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/")
 public class AccountDetailsController {
@@ -21,5 +29,35 @@ public class AccountDetailsController {
 	@GetMapping("/accountdetails")
 	public List<AccountDetails> getAllAccountDetails(){
 		return accountRepository.findAll();
+	}
+	
+	// create account rest api
+	@PostMapping("/accountdetails")
+	public AccountDetails createAccount(@RequestBody AccountDetails accountdetails) {
+		return accountRepository.save(accountdetails);
+	}
+	
+	//get employee by id rest api
+	@GetMapping("/accountdetails/{id}")
+	public ResponseEntity<AccountDetails> getAccountById(@PathVariable long id) {
+		AccountDetails accountdetails = accountRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+		return ResponseEntity.ok(accountdetails);
+	}
+	
+	// update account rest API 
+	@PutMapping("/accountdetails/{id}")
+	public ResponseEntity<AccountDetails> updateAccount(@PathVariable long id, @RequestBody AccountDetails accountupdate){
+		AccountDetails accountdetails = accountRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+		accountdetails.setName(accountupdate.getName());
+		accountdetails.setMobileNumber(accountupdate.getMobileNumber());
+		accountdetails.setAccountType(accountupdate.getAccountType());
+		accountdetails.setAccountNumber(accountupdate.getAccountNumber());
+		accountdetails.setBalance(accountupdate.getBalance());
+		accountdetails.setCurrency(accountupdate.getCurrency());
+		
+		AccountDetails updatedAccount = accountRepository.save(accountdetails);
+		return ResponseEntity.ok(updatedAccount);
 	}
 }
